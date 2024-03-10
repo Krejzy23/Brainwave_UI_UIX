f25c555f84e4279f0e8016aaeeb66932dc6bde28
Aleš Krejzl <ales.krejzl@Ales-MacBook-Air.local>
2024-03-10 13:04:40 +0100

    finalle

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -1,76 +1,12 @@
+import { benefits } from "../constants/index";
 import Heading from "./Heading";
-import { benefitIcon1,benefitIcon2,benefitIcon3,benefitIcon4 } from "../assets";
-import { benefitImage2 } from "../assets";
 import Section from "./Section";
 import Arrow from "../assets/svg/Arrow";
 import { GradientLight } from "./design/Benefits";
 import ClipPath from "../assets/svg/ClipPath";
 import { motion } from 'framer-motion';
 import { fadeIn , staggerContainer } from '../../utils/motion';
-import benefitCard1 from "../assets/benefits/card-1.svg";
-import benefitCard2 from "../assets/benefits/card-2.svg";
-import benefitCard3 from "../assets/benefits/card-3.svg";
-import benefitCard4 from "../assets/benefits/card-4.svg";
-import benefitCard5 from "../assets/benefits/card-5.svg";
-import benefitCard6 from "../assets/benefits/card-6.svg";
 
-
-export const benefits = [
-  {
-    id: "0",
-    title: "Ask anything",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
-    backgroundUrl: benefitCard1,
-    iconUrl: benefitIcon1,
-    imageUrl: benefitImage2,
-    light: false,
-  },
-  {
-    id: "1",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
-    backgroundUrl: benefitCard2,
-    iconUrl: benefitIcon2,
-    imageUrl: benefitImage2,
-    light: true,
-  },
-  {
-    id: "2",
-    title: "Connect everywhere",
-    text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
-    backgroundUrl: benefitCard3,
-    iconUrl: benefitIcon3,
-    imageUrl: benefitImage2,
-    light: false,
-  },
-  {
-    id: "3",
-    title: "Fast responding",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
-    backgroundUrl: benefitCard4,
-    iconUrl: benefitIcon4,
-    imageUrl: benefitImage2,
-    light: true,
-  },
-  {
-    id: "4",
-    title: "Ask anything",
-    text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
-    backgroundUrl: benefitCard5,
-    iconUrl: benefitIcon1,
-    imageUrl: benefitImage2,
-    light: false,
-  },
-  {
-    id: "5",
-    title: "Improve everyday",
-    text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
-    backgroundUrl: benefitCard6,
-    iconUrl: benefitIcon2,
-    imageUrl: benefitImage2,
-    light: false,
-  },
-];
 
 
 const Benefits = () => {
