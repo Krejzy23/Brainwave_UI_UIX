cfe5157770135a636972c048c5c6d5b6cefa2da4
Aleš Krejzl <ales.krejzl@Ales-MacBook-Air.local>
2024-03-10 19:08:38 +0100

    finalle

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -27,12 +27,12 @@ const Benefits = () => {
 
         <div className="flex flex-wrap gap-10 mb-10">
           {benefits.map((item , index) => (
-            <div
+            <motion.div
               variants={fadeIn('up', 'spring', index * 0.5, 1)}
               initial="hidden"
               whileInView="show"
               viewport={{once: false, amount: 0.25}}
-              className="block relative p-0.5 bg-no-repeat bg-[length:100%_100%] md:max-w-[24rem]"
+              className="block relative p-0.5  bg-[length:100%_100%] md:max-w-[24rem]"
               style={{ backgroundImage: `url(${item.backgroundUrl})` }}
               key={item.id}
             >
@@ -73,7 +73,7 @@ const Benefits = () => {
               </div>
 
               <ClipPath />
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
